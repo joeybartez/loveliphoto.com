3181fe8fc43dea9aff84d98c654c9d6e339bb9ec
Ben Li <liebennyboy@gmail.com>
2020-12-17 15:04:33 -0600

    A lot of stuff

---
+++ about.js
@@ -0,0 +1,34 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	resizeAbout();
+	window.addEventListener('resize', resizeAbout);
+	document.getElementById('meet-betty').addEventListener('click', meetBetty);
+	document.getElementById('meet-betty-back').addEventListener('click', meetBetty);
+	document.getElementById('meet-sharon').addEventListener('click', meetSharon);
+	document.getElementById('meet-sharon-back').addEventListener('click', meetSharon);
+});
+
+function resizeAbout() {
+	let portfolio = document.getElementById('portfolio');
+	let yScrollBarWidth = window.innerWidth - document.body.offsetWidth;
+	document.documentElement.style.setProperty('--y-scroll-bar-width', yScrollBarWidth + 'px');
+}
+
+function meetBetty(event) {
+	document.getElementById('meet-betty').classList.toggle('hidden');
+	document.getElementById('meet-betty-back').classList.toggle('hidden');
+
+	var classList = document.getElementById('about').classList;
+	classList.toggle('about-center');
+	classList.toggle('about-left');
+}
+
+function meetSharon(event) {
+	document.getElementById('meet-sharon').classList.toggle('hidden');
+	document.getElementById('meet-sharon-back').classList.toggle('hidden');
+
+	var classList = document.getElementById('about').classList;
+	classList.toggle('about-center');
+	classList.toggle('about-right');
+}
---
+++ email.js
@@ -0,0 +1,46 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	document.getElementById('contact-form').addEventListener('submit', sendEmail);
+});
+
+function sendEmail(event) {
+	event.preventDefault();
+
+	let input = event.currentTarget;
+	let submit = input[5];
+	submit.value = "SENDING...";
+	submit.style.border = "thick solid #3E3E3E";
+	submit.style.background = "#3E3E3E";
+	submit.style.color = "#fcd2d3";
+
+	let name = input[0].value;
+	let phone = input[1].value;
+	let email = input[2].value;
+	let subject = input[3].value;
+	let message = input[4].value;
+
+	let body = "NAME: " + name + "<br>" +
+	"EMAIL: " + email + "<br>" +
+	"PHONE: " + phone + "<br>" +
+	"<br>" + "MESSAGE:" + "<br>" + message;
+	
+	//SmtpJS.com
+	Email.send({
+		SecureToken: "b0b558c9-5dc9-4680-9863-300d8f2675e2",
+		To: "liebennyboy@gmail.com",
+		From: "betty@loveliphoto.com",
+		Subject: subject,
+		Body: body
+	}).then(emailCallback);
+}
+
+function emailCallback(message) {
+	let submit = document.getElementById("contact-form").elements["submit"];
+
+	if (message == "OK") {
+		submit.value = "SENT!"
+	} else {
+		submit.value = "ERROR PLEASE TRY AGAIN"
+	}
+}
